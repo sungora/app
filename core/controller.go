8bbef38090b83162a43d03bcf7c2035857a0de8c
Konstantin <konstantin@shamiev.ru>
2018-10-24 14:04:09 +0300

    factory controller

--- core/controller.go
+++ core/controller.go
@@ -15,3 +15,34 @@ type ControllerFace interface {
 	Render()
 }
 
+type Controller struct {
+	Request  *request
+	Response *response
+	Data     interface{}
+}
+
+func (self *Controller) Init(w http.ResponseWriter, r *http.Request) {
+	self.Request = newrequest(r)
+	self.Response = newresponse(w)
+}
+func (self *Controller) GET() (err error) {
+	return
+}
+func (self *Controller) POST() (err error) {
+	return
+}
+func (self *Controller) PUT() (err error) {
+	return
+}
+func (self *Controller) DELETE() (err error) {
+	return
+}
+func (self *Controller) OPTIONS() (err error) {
+	return
+}
+func (self *Controller) Render() {
+	if self.Response.responseStatus {
+		return
+	}
+	self.Response.Json200(self.Data)
+}
