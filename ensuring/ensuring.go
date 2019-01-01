6eed243336584f80e464dbfa76a2d14258d23382
kshamiev <konstantin@shamiev.ru>
2019-01-02 00:53:50 +0300

    ensuring cut

--- ensuring/ensuring.go
+++ ensuring/ensuring.go
@@ -3,16 +3,49 @@ package ensuring
 import (
 	"errors"
 	"fmt"
+	"os"
 	"strings"
 	"syscall"
 )
 
-var fl FLock
+type fLock struct {
+	fh *os.File
+}
+
+func newFLock(path string) (fLock, error) {
+	var ret fLock
+	var err error
+
+	ret.fh, err = os.OpenFile(path, os.O_CREATE|os.O_RDWR, 0600)
+	return ret, err
+}
+
+func (lock fLock) Lock() error {
+	return syscall.Flock(int(lock.fh.Fd()), syscall.LOCK_EX)
+}
+
+func (lock fLock) TryLock() (bool, error) {
+	err := syscall.Flock(int(lock.fh.Fd()), syscall.LOCK_EX|syscall.LOCK_NB)
+	switch err {
+	case nil:
+		return true, nil
+	case syscall.EWOULDBLOCK:
+		return false, nil
+	}
+	return false, err
+}
+
+func (lock fLock) Unlock() error {
+	lock.fh.Close()
+	return syscall.Flock(int(lock.fh.Fd()), syscall.LOCK_UN)
+}
+
+var fl fLock
 
 // PidFileCreate Создание PID файла с ID текущего процесса
 func PidFileCreate(fileName string) (err error) {
 	var unlocked bool
-	fl, err = NewFLock(fileName)
+	fl, err = newFLock(fileName)
 	if err == nil {
 		unlocked, err = fl.TryLock()
 		if unlocked {
