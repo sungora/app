efa6d9f997523b8c29c4d23876798d4570f4543a
kvshamiy <kvshamiy@mts.ru>
2019-03-14 12:18:49 +0300

    bugfix connect charset DB postgresql

--- connect/init.go
+++ connect/init.go
@@ -34,13 +34,14 @@ func Init(cfg *Config) (com *Component, err error) {
 		}
 	} else if config.Postgresql.Host != "" {
 		if db, err = gorm.Open("postgres", fmt.Sprintf(
-			"host=%s port=%d user=%s dbname=%s password=%s sslmode=%s",
+			"host=%s port=%d user=%s dbname=%s password=%s sslmode=%s charset=%s",
 			config.Postgresql.Host,
 			config.Postgresql.Port,
 			config.Postgresql.Login,
 			config.Postgresql.Name,
 			config.Postgresql.Password,
 			config.Postgresql.Ssl,
+			config.Postgresql.Charset,
 		)); err != nil {
 			return
 		}
