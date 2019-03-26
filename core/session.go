dbb37466eabdb0a6a10ef6ed7748affa59670c75
kshamiev <konstantin@shamiev.ru>
2019-03-26 13:14:26 +0300

    rename typ Session public

--- core/session.go
+++ core/session.go
@@ -9,36 +9,36 @@ func sessionGC() {
 	go func() {
 		for {
 			time.Sleep(time.Minute * 1)
-			for i, s := range session {
+			for i, s := range sessions {
 				if Cfg.SessionTimeout < time.Now().In(Cfg.TimeLocation).Sub(s.t) {
-					delete(session, i)
+					delete(sessions, i)
 				}
 			}
 		}
 	}()
 }
 
-var session = make(map[string]*sessionTyp)
+var sessions = make(map[string]*Session)
 
-type sessionTyp struct {
+type Session struct {
 	t    time.Time
 	data map[string]interface{}
 }
 
 // GetSession Получение сессии
-func GetSession(token string) *sessionTyp {
-	if elm, ok := session[token]; ok {
+func GetSession(token string) *Session {
+	if elm, ok := sessions[token]; ok {
 		elm.t = time.Now().In(Cfg.TimeLocation)
 		return elm
 	}
-	session[token] = new(sessionTyp)
-	session[token].t = time.Now().In(Cfg.TimeLocation)
-	session[token].data = make(map[string]interface{})
-	return session[token]
+	sessions[token] = new(Session)
+	sessions[token].t = time.Now().In(Cfg.TimeLocation)
+	sessions[token].data = make(map[string]interface{})
+	return sessions[token]
 }
 
 // Get получение данных сессии
-func (s *sessionTyp) Get(index string) interface{} {
+func (s *Session) Get(index string) interface{} {
 	if _, ok := s.data[index]; ok {
 		return s.data[index]
 	}
@@ -46,11 +46,11 @@ func (s *sessionTyp) Get(index string) interface{} {
 }
 
 // Set сохранение данных в сессии
-func (s *sessionTyp) Set(index string, value interface{}) {
+func (s *Session) Set(index string, value interface{}) {
 	s.data[index] = value
 }
 
 // Del удаление данных из сессии
-func (s *sessionTyp) Del(index string) {
+func (s *Session) Del(index string) {
 	delete(s.data, index)
 }
